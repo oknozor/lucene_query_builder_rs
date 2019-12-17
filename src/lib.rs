d21b66c60998604592cde8b9aaa3930ab94ca3c0
Paul Delafosse <paul.delafosse@protonmail.com>
2019-12-17 10:18:16 +0100

    refactor: move common generated functions to dedicated module

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,7 @@
 extern crate proc_macro;
 
+mod gen;
+
 use proc_macro::TokenStream;
 use quote::format_ident;
 use quote::quote;
@@ -64,6 +66,7 @@ pub fn derive(input: TokenStream) -> TokenStream {
         .cloned()
         .map(|name| format!("{}", name))
         .collect();
+    let common_functions = gen::common_functions();
 
     let output = quote! {
         use std::fmt;
@@ -139,48 +142,7 @@ pub fn derive(input: TokenStream) -> TokenStream {
                 self
             })*
 
-            pub fn or(&mut self) -> &mut Self {
-                if let Some(last) = self.query.last_mut() {
-                    last.1 = Operator::Or;
-                };
-
-                self.query.push(("".into(), Operator::End));
-                self
-            }
-
-            pub fn and(&mut self) -> &mut Self {
-                if let Some(last) = self.query.last_mut() {
-                    last.1 = Operator::And;
-                };
-
-                self.query.push(("".into(), Operator::End));
-                self
-            }
-
-            pub fn proximity(&mut self, proximity: i32) -> &mut Self {
-                if let Some(last) = self.query.last_mut() {
-                    let prox = &format!("~{}", proximity);
-                    last.0.push_str(prox);
-                };
-
-                self
-            }
-
-            pub fn build(&mut self) -> String {
-                format!("query={}", self.to_string_query())
-            }
-
-            pub fn expr(&mut self, exp: &mut Self) -> &mut Self {
-                let value = format!("({})", exp.to_string_query());
-                self.query.push((value, Operator::End));
-                self
-            }
-
-            fn to_string_query(&self) -> String {
-                self.query.iter()
-                    .map(|(value, separator)| format!("{}{}", value, separator))
-                    .collect::<String>()
-            }
+            #common_functions
         }
     };
 
